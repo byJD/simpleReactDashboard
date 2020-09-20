273846ccc4be1cc4cc31da27b17118a8634e8f33
Jochem Dierx <jochem.dierx@gmail.com>
2020-09-20 15:03:50 -0700

    progress?

--- simple_dashboard/src/App.js
+++ simple_dashboard/src/App.js
@@ -1,48 +1,95 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect  } from 'react';
+
+import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
+import * as d3 from "d3";
 
 // import Icon from './FirstChart'
 import BarChart from './BarChart'
 import SecondChart from './SecondChart'
 import BarChart2 from './BarChart2'
 import country  from './country'
+import Pie from './PieChart'
+import { SimplePieChart } from "./SimplePieChart"
+import BarChart3 from './BarChart3'
+import BitcoinChart from './BitcoinChart'
 
 
-const   data = [
-  [10, 30, 40, 20],
-  [10, 40, 30, 20, 50, 10],
-  [60, 30, 40, 20, 30]
-]
+
+function RandomData() {
+  const data = [...Array(5)].map((e, i) => {
+    return {
+      x: Math.random() * 40,
+      y: Math.random() * 40,
+      temparature: Math.random() * 500
+    };
+  });
+  return data;
+}
+
+const data = RandomData()
 
 class App extends Component {
 
-
-  
-  
-
-
-state = {
-  data: [12, 5, 6, 6, 9, 10],
-  width: 700,
-  height: 500,
-  id: "root"
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
   }
 
-  render() {
-  return (
-    //  <div className='App'>
-    //  <div className='App-header'>
-    //  <h2>Simple Dashboard</h2>
-     <div>
-     <BarChart data={this.state.data} width={this.state.width} height={this.state.height} />
-     <SecondChart/>
-     <BarChart2 width={600} height={400} data={data} />
-     <country/>
-     </div>
-  )
+
+  //get API data for Bitcoin price
+  componentDidMount() {
+    fetch(`https://api.coindesk.com/v1/bpi/historical/close.json`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          data: Object.keys(data.bpi).map(date => {
+         return {
+            date: new Date(date),
+            price: data.bpi[date]
+           };
+        })
+      });
+    })
+     .catch(error => console.log(error));
   }
-}
+
+    // state = {
+    //   data1: [12, 5, 6, 6, 9, 10],
+    //   width: 700,
+    //   height: 500,
+    //   id: "root"
+    //   }
+
+    render() {
+    const { data } = this.state;
+    return (
+      //  <div className='App'>
+      //  <div className='App-header'>
+      //  <h2>Simple Dashboard</h2>
+      <div>
+      {/* <BarChart data={this.state.data1} width={this.state.width} height={this.state.height} /> */}
+      <SecondChart/>
+      {/* <BarChart2 width={600} height={400} data={data} /> */}
+      <country/>
+      <SimplePieChart />
+      <BitcoinChart data={data} />
+      <BarChart3/>
+      <Pie
+            data={data}
+            width={200}
+            height={200}
+            innerRadius={60}
+            outerRadius={100}
+          />
+      </div>
+      
+    )
+  }
+};
 
 export default App;
 
---
+++ simple_dashboard/src/BarChart3.js
@@ -0,0 +1,71 @@
+
+import React, {Component} from 'react'
+import './App.css'
+
+import { scaleLinear } from 'd3-scale'
+import { extent } from 'd3-array'
+import { select } from 'd3-selection'
+
+import * as d3 from 'd3'
+
+//using React to draw the chart, and D3 for the calculations 
+
+class BarChart3 extends Component {
+
+    componentDidMount() {
+        const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+        const w = 600;
+        const h = 300;
+
+        const svg = d3
+        .select(this.refs.chart)
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .attr("class", "bar");
+
+        svg
+        .selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("fill", "navy")
+        .attr("class", "sBar")
+        .attr("x", (d, i) => i * 60)
+        .attr("y", (d, i) => {
+            return h - 7 * d;
+        })
+        .attr("width", 50)
+        .attr("height", (d, i) => 7 * d)
+        .append("title")
+        .text(d => d);
+
+        svg
+        .selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .style("font-size", 18)
+        .attr("fill", "red")
+        .attr("x", (d, i) => i * 60)
+        .attr("y", (d, i) => h - 7 * d - 3)
+        .text(d => d);
+    }
+    render() {
+        const styles = {
+        container: {
+            display: "grid",
+            justifyItems: "center"
+        }
+        };
+        return (
+        <div ref="chart" style={styles.container}>
+            <h1 style={{ textAlign: "center" }}>Hi, I'm the bar chart :)</h1>
+        </div>
+        );
+    }
+}
+
+
+export default BarChart3;
